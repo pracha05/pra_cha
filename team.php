4a68d1cfe513e0ae67f83fc6321a1e5db0d17174
anilkumar9989 <51983070+anilkumar9989@users.noreply.github.com>
2019-07-01 12:27:16 +0530

    cc

--- team.php
+++ team.php
@@ -9,7 +9,11 @@
     background: #233059;
     border-bottom: 1px #12192f solid;
     box-shadow: 0 3px 2px rgba(0, 0, 0, 0.04);
-}
+	}
+	.email-size{
+		font-size:12px;
+		}
+		
 	   </style>
 
 <!-- ==============================================
@@ -32,9 +36,9 @@
 		  
             <figure><img src="images/team/pushkar.png" class="img-responsive" alt="pushkar"></figure>
             <h3>Pushkar v</h3>
-            <p>Director / Founder</p>
+            <p style="margin:0">Director / Founder</p>
 			<div>Mobile: 9494422779 </div>
-			<div>Email: pushkar@prachatech.com </div>
+			<div class="email-size">Email: pushkar@prachatech.com </div>
            
           </div>
       </li>   
@@ -42,7 +46,9 @@
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/avinash.png" class="img-responsive" alt="pushkar"></figure>
             <h3>Avinash V </h3>
-            <p>Director</p>
+            <p style="margin:0">Director</p>
+           <div>Mobile: 94944 22114 </div>
+			<div class="email-size">Email: avinash@prachatech.com</div>
            
           </div>
       </li>  
@@ -50,21 +56,27 @@
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/arya-male.png" class="img-responsive" alt="Arya Chatterjee"></figure>
             <h3>Arya Chatterjee</h3>
-            <p>Regional Business Head</p>
+            <p style="margin:0">Regional Business Head</p>
+			<div>Mobile: 9830349568 </div>
+			<div class="email-size">Email: pushkar@prachatech.com </div>
           </div>
 		</li>
 			<li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/reddamma.png" class="img-responsive" alt="reddamma"></figure>
             <h3>Reddamma</h3>
-            <p>HR Executive</p>
+            <p style="margin:0">HR Executive</p>
+			<div>Mobile: 8546920198 </div>
+			<div class="email-size">Email: hr@prachatech.com </div>
           </div>
 		</li>
 		<li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/rita.png" class="img-responsive" alt="rita"></figure>
             <h3>Rita </h3>
-            <p>HR - Manager</p>
+            <p style="margin:0">HR - Manager</p>
+			<div>Mobile: 9949749490 </div>
+			<div class="email-size">Email: hr_rita@prachatech.com </div>
           </div>
 		</li>
 	
@@ -72,8 +84,9 @@
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/bayapu.png" class="UI Desinger" alt=""></figure>
             <h3>Bayapureddy CG</h3>
-            <p>UI Desginer</p>
-           
+            <p style="margin:0">UI Desginer</p>
+			<div>Mobile: 8500226782 </div>
+			<div class="email-size">Email: bayapureddy@prachatech.com </div>
           </div>
       </li>
  
@@ -81,16 +94,18 @@
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/vasu.png" class="img-responsive" alt="vasu"></figure>
             <h3>Vasudeva Reddy R </h3>
-            <p>Senior PHP Developer</p>
-
+            <p style="margin:0">Senior PHP Developer</p>
+			<div>Mobile: 8500050944 </div>
+			<div class="email-size">Email: vasudevareddy@prachatech.com </div>
           </div>
        </li>
       <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/siva.png" class="img-responsive" alt="siva"></figure>
             <h3>Sivakumar Reddy  </h3>
-            <p>Junior PHP Developer</p>
-          
+            <p style="margin:0">Junior PHP Developer</p>
+			<div>Mobile: 7013319036 </div>
+			<div class="email-size">Email: sivakumarreddy@prachatech.com </div>
           </div>
       </li> 
 
@@ -98,84 +113,108 @@
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/sahu.png" class="img-responsive" alt="Bhupendra"></figure>
             <h3>Bhupendra </h3>
-            <p>Android Developer</p>
+            <p style="margin:0">Android Developer</p>
+			<div>Mobile: 8319587562 </div>
+			<div class="email-size">Email: bhupendra@prachatech.com </div>
           </div>
       </li>  
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/keerthi.png" class="img-responsive" alt="keerthi"></figure>
             <h3>Keerthi Reddy </h3>
-            <p>Junior Android Developer</p>
+            <p style="margin:0">Junior Android Developer</p>
+			<div>Mobile: 8884170374 </div>
+			<div class="email-size">Email: keerthi@prachatech.com </div>
           </div>
       </li> 
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/arya.png" class="img-responsive" alt="arya"></figure>
             <h3>Arya </h3>
-            <p>Quality Analyst</p>
+            <p style="margin:0">Quality Analyst</p>
+			<div>Mobile: 955025238 </div>
+			<div class="email-size">Email: arya@prachatech.com</div>
           </div>
       </li>
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/anupama.png" class="img-responsive" alt="anupama"></figure>
             <h3>Anupama kulkarni </h3>
-            <p>Quality Analyst</p>
+            <p style="margin:0">Quality Analyst</p>
+			<div>Mobile: 9502710179 </div>
+			<div class="email-size">Email: anupama@prachatech.com </div>
           </div>
       </li>
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/raghuram.png" class="img-responsive" alt="raghuram"></figure>
             <h3>Sai Raghuram A</h3>
-            <p>Project Coordinator</p>
+            <p style="margin:0">Project Coordinator</p>
+			<div>Mobile: 9581758358 </div>
+			<div class="email-size">Email: sairaghuram@prachatech.com </div>
           </div>
       </li> 
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/praveen.png" class="img-responsive" alt="praveen"></figure>
             <h3>Praveen Kumar K</h3>
-            <p>Research Analyst</p>
+            <p style="margin:0">Research Analyst</p>
+			<div>Mobile: 9603780475</div>
+			<div class="email-size">Email: praveen@prachatech.com </div>
           </div>
       </li>
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/jagadesh.png" class="img-responsive" alt="jagadesh"></figure>
             <h3>Jadagesh</h3>
-            <p>Research Analyst</p>
+            <p style="margin:0">Research Analyst</p>
+			<div>Mobile: 9866969528 </div>
+			<div class="email-size">Email: jagadesh@prachatech.com</div>
           </div>
       </li> 
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/sathish.png" class="img-responsive" alt="sathish"></figure>
             <h3>Satish</h3>
-            <p>Inside Sales</p>
+            <p style="margin:0">Inside Sales</p>
+			<div>Mobile: 9398888512 </div>
+			<div class="email-size">Email: satish@prachatech.com </div>
           </div>
       </li> 
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive" alt=""></figure>
             <h3>Nagesh</h3>
-            <p>Inside Sales</p>
+            <p style="margin:0">Inside Sales</p>
+			<div>Mobile: 9014437007 </div>
+			<div class="email-size">Email: nagesh@prachatech.com </div>
           </div>
       </li>  
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/rayala.png" class="img-responsive" alt="Rayal Chakkani"></figure>
             <h3>Rayala Chakkani</h3>
-            <p>Tele caller</p>
+            <p style="margin:0">Tele caller</p>
+			<div>Mobile: 7675991339 </div>
+			<div class="email-size">Email: rayalchakkani@prachatech.com </div>
           </div>
       </li> 
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/sathya.png" class="img-responsive" alt="sathya"></figure>
             <h3>Satyanandam</h3>
-            <p>Digital Marketing Executive</p>
+            <p style="margin:0">Digital Marketing Executive</p>
+			<div>Mobile: 6302365137</div>
+			<div class="email-size">Email: Satyanandam@prachatech.com </div>
           </div>
       </li>  
 	  <li class="col-12 col-md-6 col-lg-3">
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive" alt=""></figure>
             <h3>Koti</h3>
-            <p>Digital Marketing Executive</p>
+            <p style="margin:0">Digital Marketing Executive</p>
+			<div>Mobile: 8978127273 </div>
+			<div class="email-size">Email: koti@prachatech.com </div>
           </div>
       </li> 
 	
@@ -184,7 +223,9 @@
           <div class="cnt-block equal-hight" style="height: 300px;">
             <figure><img src="images/team/sridhar.png" class="img-responsive" alt="sridhar"></figure>
             <h3>Sridhar</h3>
-            <p>Business Development Executive</p>
+            <p style="margin:0">Business Development Executive</p>
+			<div>Mobile: 9948871887 </div>
+			<div class="email-size">Email: sridhar@prachatech.com </div>
           </div>
 		</li>
 		
