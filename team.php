b30449b9c2455b883e3e7d7bd62c4c61e6ac81d6
anilkumar9989 <51983070+anilkumar9989@users.noreply.github.com>
2019-07-02 17:55:08 +0530

    team

--- team.php
+++ team.php
@@ -57,7 +57,6 @@
             <figure><img src="images/team/arya-male.png" class="img-responsive" alt="Arya Chatterjee"></figure>
             <h3>Arya Chatterjee</h3>
             <p style="margin:0">Regional Business Head</p>
-			<div>Mobile: 9830349568 </div>
 			<div class="email-size">Email: aryachatterjee@prachatech.com </div>
           </div>
 		</li>
@@ -66,7 +65,6 @@
             <figure><img src="images/team/reddamma.png" class="img-responsive" alt="reddamma"></figure>
             <h3>Reddamma</h3>
             <p style="margin:0">HR Executive</p>
-			<div>Mobile: 8546920198 </div>
 			<div class="email-size">Email: hr@prachatech.com </div>
           </div>
 		</li>
@@ -75,7 +73,6 @@
             <figure><img src="images/team/rita.png" class="img-responsive" alt="rita"></figure>
             <h3>Rita </h3>
             <p style="margin:0">HR - Manager</p>
-			<div>Mobile: 9949749490 </div>
 			<div class="email-size">Email: hr_rita@prachatech.com </div>
           </div>
 		</li>
@@ -85,7 +82,6 @@
             <figure><img src="images/team/bayapu.png" class="UI Desinger" alt=""></figure>
             <h3>Bayapureddy CG</h3>
             <p style="margin:0">UI Desginer</p>
-			<div>Mobile: 8500226782 </div>
 			<div class="email-size">Email: bayapureddy@prachatech.com </div>
           </div>
       </li>
@@ -95,7 +91,6 @@
             <figure><img src="images/team/vasu.png" class="img-responsive" alt="vasu"></figure>
             <h3>Vasudeva Reddy R </h3>
             <p style="margin:0">Senior PHP Developer</p>
-			<div>Mobile: 8500050944 </div>
 			<div class="email-size">Email: vasudevareddy@prachatech.com </div>
           </div>
        </li>
@@ -104,7 +99,6 @@
             <figure><img src="images/team/siva.png" class="img-responsive" alt="siva"></figure>
             <h3>Sivakumar Reddy  </h3>
             <p style="margin:0">Junior PHP Developer</p>
-			<div>Mobile: 7013319036 </div>
 			<div class="email-size">Email: sivakumarreddy@prachatech.com </div>
           </div>
       </li> 
@@ -114,7 +108,6 @@
             <figure><img src="images/team/sahu.png" class="img-responsive" alt="Bhupendra"></figure>
             <h3>Bhupendra </h3>
             <p style="margin:0">Android Developer</p>
-			<div>Mobile: 8319587562 </div>
 			<div class="email-size">Email: bhupendra@prachatech.com </div>
           </div>
       </li>  
@@ -123,7 +116,6 @@
             <figure><img src="images/team/keerthi.png" class="img-responsive" alt="keerthi"></figure>
             <h3>Keerthi Reddy </h3>
             <p style="margin:0">Junior Android Developer</p>
-			<div>Mobile: 8884170374 </div>
 			<div class="email-size">Email: keerthi@prachatech.com </div>
           </div>
       </li> 
@@ -132,7 +124,6 @@
             <figure><img src="images/team/arya.png" class="img-responsive" alt="arya"></figure>
             <h3>Arya </h3>
             <p style="margin:0">Quality Analyst</p>
-			<div>Mobile: 955025238 </div>
 			<div class="email-size">Email: arya@prachatech.com</div>
           </div>
       </li>
@@ -141,7 +132,6 @@
             <figure><img src="images/team/anupama.png" class="img-responsive" alt="anupama"></figure>
             <h3>Anupama kulkarni </h3>
             <p style="margin:0">Quality Analyst</p>
-			<div>Mobile: 9502710179 </div>
 			<div class="email-size">Email: anupama@prachatech.com </div>
           </div>
       </li>
@@ -150,7 +140,6 @@
             <figure><img src="images/team/raghuram.png" class="img-responsive" alt="raghuram"></figure>
             <h3>Sai Raghuram A</h3>
             <p style="margin:0">Project Coordinator</p>
-			<div>Mobile: 9581758358 </div>
 			<div class="email-size">Email: sairaghuram@prachatech.com </div>
           </div>
       </li> 
@@ -159,7 +148,6 @@
             <figure><img src="images/team/praveen.png" class="img-responsive" alt="praveen"></figure>
             <h3>Praveen Kumar K</h3>
             <p style="margin:0">Research Analyst</p>
-			<div>Mobile: 9603780475</div>
 			<div class="email-size">Email: praveen@prachatech.com </div>
           </div>
       </li>
@@ -168,7 +156,6 @@
             <figure><img src="images/team/jagadesh.png" class="img-responsive" alt="jagadesh"></figure>
             <h3>Jadagesh</h3>
             <p style="margin:0">Research Analyst</p>
-			<div>Mobile: 9866969528 </div>
 			<div class="email-size">Email: jagadesh@prachatech.com</div>
           </div>
       </li> 
@@ -177,7 +164,6 @@
             <figure><img src="images/team/sathish.png" class="img-responsive" alt="sathish"></figure>
             <h3>Satish</h3>
             <p style="margin:0">Inside Sales</p>
-			<div>Mobile: 9398888512 </div>
 			<div class="email-size">Email: satish@prachatech.com </div>
           </div>
       </li> 
@@ -186,7 +172,6 @@
             <figure><img src="images/team/nagesh.png" class="img-responsive" alt="nagesh "></figure>
             <h3>Nagesh</h3>
             <p style="margin:0">Inside Sales</p>
-			<div>Mobile: 9014437007 </div>
 			<div class="email-size">Email: nagesh@prachatech.com </div>
           </div>
       </li>  
@@ -195,7 +180,6 @@
             <figure><img src="images/team/rayala.png" class="img-responsive" alt="Rayal Chakkani"></figure>
             <h3>Rayala Chakkani</h3>
             <p style="margin:0">Tele caller</p>
-			<div>Mobile: 7675991339 </div>
 			<div class="email-size">Email: rayalchakkani@prachatech.com </div>
           </div>
       </li> 
@@ -204,7 +188,6 @@
             <figure><img src="images/team/sathya.png" class="img-responsive" alt="sathya"></figure>
             <h3>Satyanandam</h3>
             <p style="margin:0">Digital Marketing Executive</p>
-			<div>Mobile: 6302365137</div>
 			<div class="email-size">Email: Satyanandam@prachatech.com </div>
           </div>
       </li>  
@@ -213,7 +196,6 @@
             <figure><img src="images/team/koti.png" class="img-responsive" alt="koti"></figure>
             <h3>Koti</h3>
             <p style="margin:0">Digital Marketing Executive</p>
-			<div>Mobile: 8978127273 </div>
 			<div class="email-size">Email: koti@prachatech.com </div>
           </div>
       </li> 
@@ -224,7 +206,6 @@
             <figure><img src="images/team/sridhar.png" class="img-responsive" alt="sridhar"></figure>
             <h3>Sridhar</h3>
             <p style="margin:0">Business Development Executive</p>
-			<div>Mobile: 9948871887 </div>
 			<div class="email-size">Email: sridhar@prachatech.com </div>
           </div>
 		</li>
